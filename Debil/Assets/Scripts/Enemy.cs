5c48ec9917cffe8376581ffd3bf0c4f7f10ba7f9
Grondhog <Sgronhovd@gmail.com>
2018-07-11 21:54:14 -0700

    VERY Simple enemy AI
    Enemy follows player around.

--- Debil/Assets/Scripts/Enemy.cs
+++ Debil/Assets/Scripts/Enemy.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MovingCharacter {
 
     public Transform player;
-    
 
+    private NavMeshAgent nmAgent;
 
 	// Use this for initialization
 	void Start () {
-		
+        nmAgent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        nmAgent.SetDestination(player.position);
 	}
 }
