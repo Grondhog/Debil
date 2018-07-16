36230ca342efbb04d5ce10c5f9e91881490bf33e
Grondhog <Sgronhovd@gmail.com>
2018-07-15 21:25:36 -0700

    Enemy Manager
    Spawns enemies that follow a point that rorates around the player. Spawn
    times and adjusted will need to be adjusted.

--- Debil/Assets/Scripts/Enemy.cs
+++ Debil/Assets/Scripts/Enemy.cs
@@ -9,13 +9,65 @@ public class Enemy : MovingCharacter {
 
     private NavMeshAgent nmAgent;
 
-	// Use this for initialization
-	void Start () {
+    public float minDistance = 10f;
+    private float curMinDistance;
+
+    private int angle = 0;
+    private int angleDelay = 10;
+    private int count = 0;
+
+
+    // Use this for initialization
+    void Start () {
         nmAgent = GetComponent<NavMeshAgent>();
-	}
+        curMinDistance = minDistance;
+        nmAgent.SetDestination(player.position);
+        nmAgent.speed = 100;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        nmAgent.SetDestination(player.position);
+        count++;
+        if(count > angleDelay)
+        {
+            angle = (angle + 1) % 360;
+            count = 0;
+        }
+        //print(player.position);
+        
+        //SetDistanceMinDistanceFromTarget();
+        if(nmAgent.remainingDistance > curMinDistance)
+        {
+            nmAgent.isStopped = false;
+            nmAgent.SetDestination(player.position);
+        }
+        else
+        {
+            nmAgent.isStopped = true;
+            SetDistanceMinDistanceFromTarget();
+        }
 	}
+
+    public void Attack(bool attack)
+    {
+        if (attack)
+            curMinDistance = 1f;    
+        else
+            curMinDistance = minDistance;
+    }
+
+    private void OnDrawGizmos()
+    {
+        float x = (Mathf.Sin(Mathf.Deg2Rad * angle) * curMinDistance) + player.position.x;
+        float y = (Mathf.Cos(Mathf.Deg2Rad * angle) * curMinDistance) + player.position.z;
+        Gizmos.DrawCube(new Vector3(x, 0, y), new Vector3(1, 1 , 1));
+    }
+
+    private void SetDistanceMinDistanceFromTarget()
+    {
+        float x = (Mathf.Sin(Mathf.Deg2Rad * angle) * curMinDistance) + player.position.x;
+        float y = (Mathf.Cos(Mathf.Deg2Rad * angle) * curMinDistance) + player.position.z;
+        print("Changing destination: " + x + ", " + y);
+        nmAgent.SetDestination(new Vector3(x, 0, y));
+    }
 }
---
+++ Debil/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    List<Enemy> enemies;
+
+    public int maxEnemies = 10;
+    public int spawnDelay = 30;
+    private int framesSinceSpawn = 0;
+    public GameObject enemy;
+
+    public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+        enemies = new List<Enemy>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        framesSinceSpawn++;
+		if(enemies.Count < maxEnemies && framesSinceSpawn >= spawnDelay)
+        {
+            framesSinceSpawn = 0;
+            SpawnEnemy();
+        }
+	}
+
+    private void SpawnEnemy()
+    {
+        Enemy e = Instantiate(enemy, this.transform.position, Quaternion.identity).GetComponent<Enemy>();
+        e.player = player.transform;
+        enemies.Add(e);
+    }
+
+    public void SendEnemy(int index)
+    {
+        enemies[index].Attack(true);
+    }
+}
